e48596e6560600e9d66a3b311a349e9d2933ba6a
AlekseyGardevuar <aleksey.konashenkov1999@mail.ru>
2023-12-03 13:21:05 +0300

    Написана программа

--- задание/Program.cs
+++ задание/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿Console.Clear();
+
+string[] strings = {"qwerty", "jojo", "go", "two", "11"};
+
+string[] result = strings.Where(x => x.Length <= 3).ToArray();
+
+void PrintArray(string[] arr)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Thread.Sleep(50);
+        System.Console.Write(arr[i]);
+        if (i < arr.Length - 1)
+            System.Console.Write(", ");
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    System.Console.Write("]");
+    Console.ResetColor();
+}
+
+PrintArray(result);
